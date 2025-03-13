3495e0d2ee5598c84530f6ec59e0d77b8f77b43a
Vineet Pacheshwar <vp1472745@gmail.com>
2025-03-14 00:21:09 +0530

    i am changed the .env file port

--- RenatlHousingWebsite/frontend/src/axios.js
+++ RenatlHousingWebsite/frontend/src/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an instance with a base URL
 const api = axios.create({
-  baseURL: 'http://localhost:5000', // Your backend API base URL
+  baseURL: 'http://localhost:6000', // Your backend API base URL
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
